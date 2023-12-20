d286a0f819529c91691fda5946851b586de14dca
antonynapony <tonypoltorabatko@gmail.com>
2023-12-20 21:41:05 +0300

    Add Code

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> numbers = new List<int> { 1, 22, -3, 45, -18, 64, 2, -7, 25 };
+int lenght;
+
+var orderedNumbers = from i in numbers
+                     orderby i ascending
+                     select i;
+if (numbers.Count % 2 == 0)
+{
+    var newNumbers =  from n in orderedNumbers.TakeLast(numbers.Count/2)
+                      orderby n descending
+                      select n;
+    List<int> final = newNumbers.ToList();
+    foreach (var n in final)
+    {
+        Console.WriteLine(n * n);
+    }
+}
+else
+{
+    var newNumbers = from n in orderedNumbers.TakeLast((numbers.Count + 1) / 2)
+                     orderby n descending
+                     select n;
+    List<int> final = newNumbers.ToList();
+    foreach (var n in final)
+    {
+        Console.WriteLine(n * n);
+    }
+}
+
+
